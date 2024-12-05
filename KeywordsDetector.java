41d4ac44bfc8d8cc9fa8c423fa74d3cdb9f67376
adam11111111111111 <adamrieger116@gmail.com>
2024-12-05 23:44:44 +0200

    done

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -1,26 +1,78 @@
 public class KeywordsDetector {
     public static void main(String[] args) {
-        String[] sentences = {
-            "Our product will transform the market",
-            "Programming is both painful and engaging",
-            "This has nothing to do with machine learning",
-            "We need to leverage our core competencies",
-            "Let's talk about data and algorithms",
-            "Chatbots are great but must be used carefully",
-            "This blockchain-based solution will disrupt the industry",
-            "The team showed great Synergy in the last project",
-            "Use simple words without hype and fluff",
-            "Our new technology presents a significant paradigm shift",
-            "Effective presentations must be clear, concise, and humble"
+        String[] sentences = { "aaaaaaaaa",
+                "Our product will transform the market",
+                "Programming is both painful and engaging",
+                "This has nothing to do with machine learning",
+                "We need to leverage our core competencies",
+                "Let's talk about data and algorithms",
+                "Chatbots are great but must be used carefully",
+                "This blockchain-based solution will disrupt the industry",
+                "The team showed great Synergy in the last project",
+                "Use simple words without hype and fluff",
+                "Our new technology presents a significant paradigm shift",
+                "Effective presentations must be clear, concise, and humble"
         };
-        // Some keywords that typically signal bullshit contents in business presentations 
-        String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
+        // for (int i = 0; i < sentences.length; i++) {
+        // String[] temp = breakDownToWords(sentences[i]);
+        // for (int j = 0; j < temp.length; j++) {
+        // System.out.print(temp[j] + " ");
+        // }
+        // System.out.println("");
+        // }
+
+        // Some keywords that typically signal bullshit contents in business
+        // presentations
+        String[] keywords = { "synergy", "disrupt", "leverage", "Paradigm",
+                "transform" };
         detectAndPrint(sentences, keywords);
     }
 
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for (int i = 0; i < sentences.length; i++) {
+            String[] temp = breakDownToWords(sentences[i]);
+            for (int j = 0; j < keywords.length; j++) {
+                for (int k = 0; k < Math.min(temp.length, keywords.length); k++) {
+                    if (temp[k].equals(keywords[j])) {
+                        System.out.println(sentences[i]);
+                        k = temp.length;
+                        j = keywords.length;
+                        break;
+                    }
+                }
+            }
+        }
     }
+
+    // gets a string and returns a string array of the words in the given string in
+    // lowercase
+    public static String[] breakDownToWords(String sentence) {
+        sentence = sentence.toLowerCase();
+        String[] temp = new String[sentence.length()];
+        String[] ans;
+        int beginIndex = 0;
+        int ansLength = 0;
+        int count = 0;
+        while (count < sentence.length()) {
+            if (sentence.charAt(count) == ' ') {
+                temp[ansLength] = sentence.substring(beginIndex, count);
+                beginIndex = count + 1;
+                ansLength++;
+            }
+            count++;
+        }
+        if (count - beginIndex > 0) {
+            temp[ansLength] = sentence.substring(beginIndex, count);
+            ansLength++;
+        }
+        ans = new String[ansLength];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = temp[i];
+        }
+
+        return ans;
+    }
+
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,57 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        final int N = Integer.parseInt(args[0]);
+        boolean[] primes = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            primes[i] = true;
+        }
+
+        int count1 = 2;
+        int increment = 2;
+
+        int count2 = 0;
+        int index = 1;
+        System.out.println("Prime numbers up to " + N + ":");
+        while (increment <= N && increment != -1) {
+            System.out.println(increment);
+            while (count1 <= N) {
+                if (primes[count1] == false || count1 % increment != 0 || count1 == increment) {
+                } else {
+                    primes[count1] = false;
+                }
+                count1++;
+            }
+            increment = nextTrue(primes, increment);
+            count1 = increment;
+        }
+
+        // }
+
+        while (index < N && index != -1) {
+            index = nextTrue(primes, index);
+            if (index == -1) {
+                break;
+            }
+            count2++;
+        }
+        System.out.println("There are " + count2 + " primes between 2 and " + N + "(" +
+                (int) (((double) count2 / N) * 100) + "% are primes)");
+
     }
+
+    // gives the index of the next true value in the given boolean array
+    // if there isn't one it returns a -1
+    public static int nextTrue(boolean[] arr, int index) {
+        if (arr[index] == true) {
+            index++;
+        }
+        while (index < arr.length) {
+            if (arr[index] == true) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
 }
